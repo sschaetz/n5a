a45a6fc17746d99b6a758ef869b769516fcb3677
Sebastian Schaetz <sschaetz@butterflynetinc.com>
2017-02-26 19:26:23 -0500

    Add make_type

--- n5a/types/n5atype.py
+++ n5a/types/n5atype.py
@@ -3,3 +3,10 @@ from collections import namedtuple
 N5AType = namedtuple('N5AType', ['name', 'cpptype', 'pytype'])
 N5AValue = namedtuple('N5AValue', ['name', 'type'])
 N5ACollection = namedtuple('N5AType', ['name', 'members'])
+
+def make_type(collection):
+    # Generate list of members.
+    members = []
+    for m in collection.members:
+        members.append(m.name)
+    return namedtuple(collection.name, members)
--- test/test_types.py
+++ test/test_types.py
@@ -1,6 +1,16 @@
 import pytest
 
-from n5a import N5AType, N5AValue, N5ACollection
+from n5a import N5AType, N5AValue, N5ACollection, make_type
+
+def get_pos3d_definition():
+    # This should not fail.
+    LongInt = N5AType(name='longint', cpptype='int32_t', pytype=int)
+
+    x = N5AValue(name='x', type=LongInt)
+    y = N5AValue(name='y', type=LongInt)
+    z = N5AValue(name='z', type=LongInt)
+
+    return N5ACollection(name='Pos3D', members=[x, y, z])
 
 def test_define_incomplete_type():
     # This should fail.
@@ -13,16 +23,7 @@ def test_define_complete_type():
     assert t.name == "myname"
 
 def test_collection():
-    # This should not fail.
-    LongInt = N5AType(name='longint', cpptype='int32_t', pytype=int)
-
-    x = N5AValue(name='x', type=LongInt)
-    y = N5AValue(name='y', type=LongInt)
-    z = N5AValue(name='z', type=LongInt)
-
-    Pos3D = N5ACollection(name='Pos3D', members=[x, y , z])
-
-    z = Pos3D
+    z = get_pos3d_definition()
 
     for i, k in enumerate(z):
         if i == 0:
@@ -31,3 +32,10 @@ def test_collection():
             assert isinstance(k, list)
         elif i > 1:
             assert False
+
+def test_make_type():
+    Pos3DT = make_type(get_pos3d_definition())
+    t = Pos3DT(x=0, y=1, z=2)
+    assert t.x == 0
+    assert t.y == 1
+    assert t.z == 2
